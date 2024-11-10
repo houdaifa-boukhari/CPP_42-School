fed5b0af77e1f8360f8e82d0a4997ae400eb2a2a
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-11-10 07:34:49 +0100

    add CPP_03

--- Cpp_03/ex01/ScavTrap.hpp
+++ Cpp_03/ex01/ScavTrap.hpp
@@ -6,15 +6,24 @@
 /*   By: hel-bouk <hel-bouk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 13:14:18 by hel-bouk          #+#    #+#             */
-/*   Updated: 2024/11/05 13:14:33 by hel-bouk         ###   ########.fr       */
+/*   Updated: 2024/11/08 16:14:57 by hel-bouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
 #define SCAVTRAP_HPP
 
+#include "ClapTrap.hpp"
 
-
-
+class ScavTrap : public ClapTrap
+{
+    public:
+        ScavTrap();
+        ScavTrap(std::string name);
+        ScavTrap(ScavTrap const & scav);
+        ScavTrap & operator=(ScavTrap const & scav);
+        ~ScavTrap();
+        void challengeNewcomer();
+};
 
 #endif
