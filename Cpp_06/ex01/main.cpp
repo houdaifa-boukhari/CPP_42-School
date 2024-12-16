fd7c777d76d9b722c0cebd4cd5a559e1b1ddef40
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-12-16 20:50:17 +0100

    Start Debuging Cpp_06

--- Cpp_06/ex01/main.cpp
+++ Cpp_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hel-bouk <hel-bouk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 14:26:37 by hel-bouk          #+#    #+#             */
-/*   Updated: 2024/12/16 15:29:11 by hel-bouk         ###   ########.fr       */
+/*   Updated: 2024/12/16 20:50:05 by hel-bouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int main()
 	Data *p;
 	uintptr_t data = 10000;
 	a.id = 10;
-	std::cout << p << std::endl;
 	p = Serializer::deserialize(data);
 	std::cout << Serializer::serialize(&a) << std::endl;
 	std::cout << p << std::endl;
